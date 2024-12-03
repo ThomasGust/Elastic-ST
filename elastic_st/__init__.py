0db999149a84f9078e7dc85ae7ad389dc8471ff0
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-12-03 14:44:18 -0800

    Update __init__.py

--- elastic_st/__init__.py
+++ elastic_st/__init__.py
@@ -1,4 +1,4 @@
-from elastic_st.analysis import plot_heatmap
+from elastic_st.analysis import plot_heatmap, scatter_plot_cells
 from elastic_st.coefficient_analysis import CoefficientGraphAnalysis, compare_adjacency_matrices
 from elastic_st.data import SpatialTranscriptomicsData
 from elastic_st.features import ModelFeature, CellTypeAbundanceFeature, MetageneAbundanceFeature
